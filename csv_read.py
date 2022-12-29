e66941b056c75a8d4c9c447f9cad4cf2435df3fc
Михаил Алекс. Морозов <Mikhail@torzhok-adm.local>
2022-12-29 16:38:13 +0300

    main func was added

--- csv_read.py
+++ csv_read.py
@@ -7,6 +7,10 @@ Fiz_dictionary = {}
 Trash_dictionary = {}
 
 
+def main():
+    print("Hello World!")
+
+
 def write_csv(data, name):
     with open(name, 'w') as f:
         writer = csv.DictWriter(f, fieldnames=list(data[0].keys()), extrasaction='ignore', delimiter = ',', quoting=csv.QUOTE_NONNUMERIC)
@@ -16,49 +20,52 @@ def write_csv(data, name):
         return
 
 
-with open('Корреспонденты.csv') as f:
-    #Ur_list = []
-    #Fiz_list = []
-    #Trash_list = []
-    reader = csv.DictReader(f)
-    i=0
-    for row in reader:
-        print(row)
-        i+=1
-        print(row['ВидКорреспондента'])
-        if row['ВидКорреспондента'] == 'Юридическое лицо':
-            print('add to Ur.csv')
-            Ur_dictionary = row
-            Ur_list.append(Ur_dictionary)
-        elif row['ВидКорреспондента'] == 'Физическое лицо':
-            print('add to Fiz.csv')
-            Fiz_dictionary = row
-            Fiz_list.append(Fiz_dictionary)
-            #i+=1
-            #if i>2:
-            #    break
-        else:
-            print('add to NotValid.csv')
-            Trash_dictionary = row
-            Trash_list.append(Trash_dictionary)
-            #i+=1
-            #if i>2:
-            #    break
-          ##if i>400:
-    print('Ur_list = ')
-    print(Ur_list) 
-    write_csv(Ur_list, 'csv_write_Ur.csv')
-    print('Fiz_list = ')
-    print(Fiz_list)
-    write_csv(Fiz_list, 'csv_write_Fiz.csv')
-    print('Trash_list = ')
-    print(Trash_list)
-    write_csv(Trash_list, 'csv_write_Trash.csv')
-            ##break
-   
+def split_main_csv_to_3(name='Корреспонденты.csv'):
+
+    with open('Корреспонденты.csv') as f:
+        #Ur_list = []
+        #Fiz_list = []
+        #Trash_list = []
+        reader = csv.DictReader(f)
+        i=0
+        for row in reader:
+            print(row)
+            i+=1
+            print(row['ВидКорреспондента'])
+            if row['ВидКорреспондента'] == 'Юридическое лицо':
+                print('add to Ur.csv')
+                Ur_dictionary = row
+                Ur_list.append(Ur_dictionary)
+            elif row['ВидКорреспондента'] == 'Физическое лицо':
+                print('add to Fiz.csv')
+                Fiz_dictionary = row
+                Fiz_list.append(Fiz_dictionary)
+                #i+=1
+                #if i>2:
+                #    break
+            else:
+                print('add to NotValid.csv')
+                Trash_dictionary = row
+                Trash_list.append(Trash_dictionary)
+                #i+=1
+                #if i>2:
+                #    break
+              ##if i>400:
+        print('Ur_list = ')
+        print(Ur_list) 
+        write_csv(Ur_list, 'csv_write_Ur.csv')
+        print('Fiz_list = ')
+        print(Fiz_list)
+        write_csv(Fiz_list, 'csv_write_Fiz.csv')
+        print('Trash_list = ')
+        print(Trash_list)
+        write_csv(Trash_list, 'csv_write_Trash.csv')
+                ##break
+        return
 
 
-
+if __name__ == "__main__":
+    main()
 
 
 
